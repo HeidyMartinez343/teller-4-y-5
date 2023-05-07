7354bf3ef6db5fa8a10af78992939170ac8db313
HeidyMartinez343 <128448240+HeidyMartinez343@users.noreply.github.com>
2023-05-07 16:13:08 -0500

    Add files via upload

---
+++ project-2687351/src/main/java/co/edu/sena/project2687351/util/BasicConnectionSingleton.java
@@ -0,0 +1,21 @@
+package co.edu.sena.project2687351.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BasicConnectionSingleton {
+    private static String URL = "jdbc:mysql://localhost:3306/myapp?serverTimezone=America/Bogota";
+    private static String USER = "root";
+    private static String PASS = "123456";
+    String username = "root";
+    String password = "123456";
+    private static Connection conn;
+    public static Connection getInstance()
+            throws SQLException {
+        if (conn == null) {
+            conn = DriverManager.getConnection(URL, USER, PASS);
+        }
+        return conn;
+    }
+} // BasicConnectionSingleton
